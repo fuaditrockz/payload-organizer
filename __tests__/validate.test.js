8c49d18e96e2ab5d1c0541a9e7aebb6f7cd07e31
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-07-28 18:26:49 +0700

    [test] Update validate test for validateRegister.

--- __tests__/validate.test.js
+++ __tests__/validate.test.js
@@ -1,24 +1,27 @@
-const { validateRegular  } = require('../src/options/validate')
+const { validateRegular, validateRegister } = require('../src/options/validate')
 const { errorResponse } = require('../src/helpers')
+
+let regularData
+let registerData
 
 describe('options -> validate: true || Object', () => {
   describe(`validate -> type: "regular"`, () => {
-    test(`Validated payload`, () => {
-      const data = {
+    beforeEach(() => {
+      regularData = {
         email: 'adit@gmail.com',
         password: '123456',
         confirmation_password: '123456'
       }
-      const mock = validateRegular(data)
-      expect(mock).toMatchObject(data)
+    })
+
+    test(`Validated payload`, () => {
+      const mock = validateRegular(regularData)
+      expect(mock).toMatchObject(regularData)
     })
 
     test(`If don't have an email`, () => {
-      const data = {
-        password: '123456',
-        confirmation_password: '123456'
-      }
-      const mock = validateRegular(data)
+      regularData.email = null
+      const mock = validateRegular(regularData)
       expect(mock).toMatchObject(errorResponse(
         'Email is empty',
         'No email'
@@ -26,11 +29,8 @@ describe('options -> validate: true || Object', () => {
     })
 
     test(`If don't have an password`, () => {
-      const data = {
-        email: 'adit@gmail.com',
-        confirmation_password: '123456'
-      }
-      const mock = validateRegular(data)
+      regularData.password = null
+      const mock = validateRegular(regularData)
       expect(mock).toMatchObject(errorResponse(
         'Password is empty',
         'No password'
@@ -38,28 +38,90 @@ describe('options -> validate: true || Object', () => {
     })
 
     test(`If email value not an email`, () => {
-      const data = {
-        email: 'aditgmail.com',
-        password: '123456',
-        confirmation_password: '123456'
-      }
-      const mock = validateRegular(data)
+      regularData.email = 'aditmail.com'
+      const mock = validateRegular(regularData)
       expect(mock).toMatchObject(errorResponse(
-        `${data.email} is not an email`,
-        data.email
+        `${regularData.email} is not an email`,
+        regularData.email
       ))
     })
 
     test(`If password doesn't match`, () => {
-      const data = {
-        email: 'adit@gmail.com',
-        password: '12345',
-        confirmation_password: '123456'
-      }
-      const mock = validateRegular(data)
+      regularData.password = '12345'
+      const mock = validateRegular(regularData)
       expect(mock).toMatchObject(errorResponse(
         `Password doesn't match`,
-        data.confirmation_password
+        regularData.confirmation_password
+      ))
+    })
+  })
+
+  describe(`validate -> type: "register"`, () => {
+    beforeEach(() => {
+      registerData = {
+        first_name: 'Fuadit',
+        last_name: 'Muhammad',
+        email: 'adit@gmail.com',
+        password: '123456',
+        confirmation_password: '123456'
+      }
+    })
+    test(`Validated payload`, () => {
+      const mock = validateRegister(registerData)
+      expect(mock).toMatchObject(registerData)
+    })
+
+    test(`If don't have a first name`, () => {
+      registerData.first_name = null
+      const mock = validateRegister(registerData)
+      expect(mock).toMatchObject(errorResponse(
+        'Email is empty',
+        'No email'
+      ))
+    })
+
+    test(`If don't have a last name`, () => {
+      registerData.last_name = null
+      const mock = validateRegister(registerData)
+      expect(mock).toMatchObject(errorResponse(
+        'Email is empty',
+        'No email'
+      ))
+    })
+
+    test(`If don't have an email`, () => {
+      registerData.email = null
+      const mock = validateRegister(registerData)
+      expect(mock).toMatchObject(errorResponse(
+        'Email is empty',
+        'No email'
+      ))
+    })
+
+    test(`If don't have an password`, () => {
+      registerData.password = null
+      const mock = validateRegister(registerData)
+      expect(mock).toMatchObject(errorResponse(
+        'Password is empty',
+        'No password'
+      ))
+    })
+
+    test(`If email value not an email`, () => {
+      registerData.email = 'aditmail.com'
+      const mock = validateRegister(registerData)
+      expect(mock).toMatchObject(errorResponse(
+        `${registerData.email} is not an email`,
+        registerData.email
+      ))
+    })
+
+    test(`If password doesn't match`, () => {
+      registerData.password = '12345'
+      const mock = validateRegister(registerData)
+      expect(mock).toMatchObject(errorResponse(
+        `Password doesn't match`,
+        registerData.confirmation_password
       ))
     })
   })
